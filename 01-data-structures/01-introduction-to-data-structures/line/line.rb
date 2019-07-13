2156ab5de4c0292cf1b3e6fd828f9d820b2533db
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-07-12 21:57:28 -0300

    linkedlist1

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -8,26 +8,40 @@ class Line
     self.members = []
   end
 
+# When you enter the line, you are placed at the back.
   def join(person)
+    members.push(person);
   end
-
+# People may leave the line whenever they see fit and those behind them take their place.
   def leave(person)
+    members.delete(person);
   end
-
+# People at front
   def front
-  end
+  front =  members.length -1;
 
+  end
+# People in the middle
   def middle
+    middle = members.length/2;
   end
-
+# People in the back
   def back
+    back = members.length;
   end
-
+# Searching People
   def search(person)
+    for each |member|
+      if member == person;
+        member[person] + 1;
+        newPerson = person;
+      }
+    }
+
   end
 
   private
-
+# Counting members by the index
   def index(person)
   end
 
